9fb43b5b373c6d823dff9375a19d1644f2a0a042
SEMykhailo <misha15-2009@ukr.net>
2022-07-18 20:28:45 +0300

    add sum function

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def sum(a,b):
+    return a+b
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(sum(3,5))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print('hi')
